2e4fd1f2fc63786cdd93dd21f1aa4814b0cbe0fd
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2025-10-09 11:44:46 +0000

    1.2 setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="whittlehurst",
-    version="1.1",
+    version="1.2",
     author="Bálint Csanády",
     python_requires='>3.6',
     author_email="csbalint@protonmail.ch",
@@ -23,5 +23,5 @@ setup(
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Mathematics",
     ],
-    install_requires=["numpy", "scipy"],
+    install_requires=["numpy", "scipy", "numba==0.62"],
 )
